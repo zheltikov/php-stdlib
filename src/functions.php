9c2c7e251f709c6c80dd04546373f3b272c6b3bb
Max Zheltikov <maxcruer@gmail.com>
2021-10-13 18:56:21 +0300

    added some functions

--- src/functions.php
+++ src/functions.php
@@ -2,5 +2,34 @@
 
 namespace Zheltikov\StdLib;
 
-// ...
+/**
+ * @param array $array
+ * @param callable $callback
+ * @return bool
+ */
+function array_every(array $array, callable $callback): bool
+{
+    foreach ($array as $key => $value) {
+        if (!call_user_func($callback, $value, $key, $array)) {
+            return false;
+        }
+    }
 
+    return true;
+}
+
+/**
+ * @param array $array
+ * @param callable $callback
+ * @return bool
+ */
+function array_some(array $array, callable $callback): bool
+{
+    foreach ($array as $key => $value) {
+        if (call_user_func($callback, $value, $key, $array)) {
+            return true;
+        }
+    }
+
+    return false;
+}
